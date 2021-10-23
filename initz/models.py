6db46efce61e76276525acffe0a34aa5a8451ada
charcalope <47309188+charcalope@users.noreply.github.com>
2021-10-22 18:42:16 -0700

    Commit message

--- initz/models.py
+++ initz/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from accounts.models import User
 
+class Tag(models.Model):
+    tag_description = models.CharField(max_length=50)
+
 # Create your models here.
 class Initiative(models.Model):
     primary_organizer = models.ForeignKey(User, related_name='primary_organizer', on_delete=models.CASCADE)
 
     title = models.CharField(max_length=200)
-    description = models.TextField()
+
+    problem_statement = models.TextField()
+    people_impact = models.TextField()
+    legislative_scope = models.TextField()
+
+    # requires a pre-defined tag list
+    tags = models.ManyToManyField(Tag)
 
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
+    # city or state
     type = models.CharField(max_length=25)
 
     supporters = models.ManyToManyField(User, related_name='supporters')
