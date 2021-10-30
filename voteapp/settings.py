9a81bf619d9e9f39a53dee4b7bafc802f65bef59
charcalope <47309188+charcalope@users.noreply.github.com>
2021-10-29 20:34:17 -0700

    Update settings.py

--- voteapp/settings.py
+++ voteapp/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'legislatorapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'zyvtod-Qopqoj-7fabne'
+EMAIL_HOST_USER = '**********@gmail.com'
+EMAIL_HOST_PASSWORD = '**********'
